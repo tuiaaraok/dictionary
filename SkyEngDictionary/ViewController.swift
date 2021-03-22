baa5bc2a7dcd3ee01b8653545f7e5370feedc95e
Tuiaara Okoneshnikova <tuyara_ok@mail.ru>
2021-03-22 20:53:59 +0300

    Initial Commit

---
+++ SkyEngDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyEngDictionary
+//
+//  Created by Туйаара Оконешникова on 22.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
